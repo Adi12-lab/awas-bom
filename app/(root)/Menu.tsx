38be33de2e77cbe29f75a4c8e2fc0c771b1d9064
Adi Laptop Redmi <adiiskandarubaidah@gmail.com>
2023-08-16 15:33:47 +0700

    Git deploy 2

--- app/(root)/Menu.tsx
+++ app/(root)/Menu.tsx
@@ -1,5 +1,4 @@
 import Image from "next/image"
-import Logo from "@/public/image/logo-remove.png"
 import Link from "next/link"
 
 
@@ -7,7 +6,7 @@ export default function Menu() {
     return (
         <div className={`h-screen flex flex-col items-center justify-center`}>
             <div>
-                <Image src={Logo} alt="Logo" className="w-[500px] h-[300px]" />
+                <Image src="/image/logo-remove.png" width={500} height={300} alt="Logo" />
             </div>
             <ul className="flex flex-col items-center gap-y-6">
                 <li><Link href="/gameplay" className="button-77" >Play game</Link></li>
---
+++ app/credits/page.tsx
@@ -0,0 +1,14 @@
+export default function Home() {
+    return (
+        <div className="flex flex-col text-white">
+
+            <a href="https://www.freepik.com/free-vector/comic-speech-bubble-with-bada-boom-text_13577376.htm#page=7&query=bomb&position=16&from_view=search&track=sph">Image by brgfx</a> on Freepik
+
+            <a href="https://www.freepik.com/free-vector/neon-grid-background_4394259.htm#page=2&query=game%20background&position=25&from_view=search&track=ais">Image by rawpixel.com</a> on Freepik
+
+            <a target="_blank" href="https://icons8.com/icon/zxH5cgKHOYLO/help">Question</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
+
+            <a target="_blank" href="https://icons8.com/icon/OTM3rFnrTNYe/bomb">Bomb</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
+        </div>
+    )
+}
